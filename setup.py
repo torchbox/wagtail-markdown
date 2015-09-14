431f4e8cf1f1dc0dd87ca579936d538188a6484e
Felicity Tarnell <ft@le-Fay.ORG>
2015-09-14 15:05:19 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env/python
+# vim:sw=4 ts=4 et:
+#
+# Copyright (c) 2015 Torchbox Ltd.
+# felicity@torchbox.com 2015-09-14
+#
+# Permission is granted to anyone to use this software for any purpose,
+# including commercial applications, and to alter it and redistribute it
+# freely. This software is provided 'as-is', without any express or implied
+# warranty.
+#
+
+from setuptools import setup, find_packages
+
+setup(
+    name='wagtailmarkdown',
+    version='0.1',
+    description='Markdown support for Wagtail',
+    long_description="Provides Markdown page field and streamfield block for Wagtail.  More info: https://github.com/torchbox/wagtail-markdown/blob/master/README.md",
+    author='Felicity Tarnell',
+    author_email='felicity@torchbox.com',
+    url='https://github.com/torchbox/wagtail-markdown',
+
+    install_requires=['Markdown==2.6.2'],
+    license='zlib license',
+
+    packages=find_packages(),
+
+    include_package_data=True,
+    package_data={},
+
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'License :: OSI Approved :: zlib/libpng License',
+    ],
+)
