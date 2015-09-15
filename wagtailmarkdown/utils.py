ae08b25ac9ca027a442a9b94983ce169ef373936
Felicity Tarnell <ft@le-fay.org>
2015-09-15 01:22:21 +0100

    render: add table tags + more css attributes

--- wagtailmarkdown/utils.py
+++ wagtailmarkdown/utils.py
@@ -34,11 +34,19 @@ def render(text):
             },
             output_format='html5'),
         tags = [ 'p', 'div', 'span', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'tt', 'pre',
-                 'em', 'strong', 'ul', 'li', 'dl', 'dd', 'dt', 'code', 'img', 'a', ],
+                 'em', 'strong', 'ul', 'li', 'dl', 'dd', 'dt', 'code', 'img', 'a',
+                 'table', 'tr', 'th', 'td', 'tbody', 'caption', 'colgroup', 'thead',
+                 'tfoot', ],
         attributes = {
-            '*': [ 'class', 'style', ],
+            '*': [ 'class', 'style', 'id', ],
             'a': [ 'href', 'target', 'rel', ],
             'img': [ 'src', 'alt', ],
+            'td', 'tr': [ 'rowspan', 'colspan', ],
         },
-        styles = [ 'color', 'background-color', 'font-family', 'font-weight', 'font-size', ]
+        styles = [ 'color', 'background-color', 'font-family', 'font-weight', 'font-size',
+                   'width', 'height', 'text-align', 
+                   'border', 'border-top', 'border-bottom', 'border-left', 'border-right',
+                   'padding', 'padding-top', 'padding-bottom', 'padding-left', 'padding-right',
+                   'margin', 'margin-top', 'margin-bottom', 'margin-left', 'margin-right',
+                   ]
         ))
