d467e0a8d2b57c5c322f71e4887d2c129c6bab83
Philipp Bosch <hello@pb.io>
2016-09-26 15:29:06 +0200

    Customize simplemde icons, disable spellchecker.

--- wagtailmarkdown/static/wagtailmarkdown/js/simplemde.attach.js
+++ wagtailmarkdown/static/wagtailmarkdown/js/simplemde.attach.js
@@ -16,6 +16,13 @@ function simplemdeAttach(id) {
         var mde = new SimpleMDE({
             element: document.getElementById(id),
             autofocus: false,
+            spellChecker: false,
+            toolbar: [
+              'bold', 'italic', 'heading-1', 'heading-2', 'heading-3', '|',
+              'quote', 'unordered-list', 'ordered-list', '|',
+              'link', '|',
+              'fullscreen', 'guide'
+            ],
         });
         mde.render();
 
