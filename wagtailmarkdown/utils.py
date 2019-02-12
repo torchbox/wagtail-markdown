a60378d10447245859268ed8b3175f8e3abf2c33
Adrian Brunyate <brunyate@gmail.com>
2019-02-11 21:54:56 -0500

    Allow users to specify additional allowed HTML.

--- wagtailmarkdown/utils.py
+++ wagtailmarkdown/utils.py
@@ -7,6 +7,7 @@
 # freely. This software is provided 'as-is', without any express or implied
 # warranty.
 #
+import importlib
 import warnings
 
 from django.utils.encoding import smart_text
@@ -26,7 +27,7 @@ def render_markdown(text, context=None, extensions=''):
     if context is None or not isinstance(context, dict):
         context = {}
     markdown_html = _transform_markdown_into_html(text, extensions)
-    sanitised_markdown_html = _sanitise_markdown_html(markdown_html)
+    sanitised_markdown_html = _sanitise_markdown_html(markdown_html, extensions)
     return mark_safe(sanitised_markdown_html)
 
 
@@ -36,11 +37,65 @@ def _transform_markdown_into_html(text, extensions):
     return markdown.markdown(smart_text(text), **markdown_kwargs)
 
 
-def _sanitise_markdown_html(markdown_html):
-    return bleach.clean(markdown_html, **_get_bleach_kwargs())
+def _sanitise_markdown_html(markdown_html, extensions):
+    """Ask the extensions in the string extensions what extra markup they allow
+       and combine with default keyword arguments to bleach.clean.
+
+       Note that we only process the 'tags', 'attributes', and 'styles' keywords.
+    """
+    bleach_kwargs = _get_base_bleach_kwargs()
+    for extension_name in extensions.split():
+        extra = _get_extension_bleach_kwargs(extension_name)
+        if "tags" in extra:
+            for tag in extra["tags"]:
+                if tag not in bleach_kwargs["tags"]:
+                    bleach_kwargs["tags"].append(tag)
+        if "attributes" in extra:
+            for tag, attrs in extra["attributes"].items():
+                for att in attrs:
+                    if att not in bleach_kwargs["attributes"][tag]:
+                        bleach_kwargs["attributes"][tag].append(att)
+        if "styles" in extra:
+            for style in extra["styles"]:
+                if style not in bleach_kwargs["styles"]:
+                    bleach_kwargs["styles"].append(style)
+
+    return bleach.clean(markdown_html, **bleach_kwargs)
 
 
-def _get_bleach_kwargs():
+def _get_extension_bleach_kwargs(extension_name):
+    """Use the lookup algorithm for python-markdown to find an extension class or module.
+
+       If the extension specifier gives a class (is 'module:classname') we look
+       for class variable 'allowed_markup'.
+       If the extension specifier if a module we look for a module level
+       variable 'allowed_markup'.
+       In each case the result is a dictionary of keyword args that will be
+       recursively merged with the defaults (see the calling function,
+       _sanitize_markdown_html).
+
+       If allowed_markup is not found return {}.
+
+       We do not currently recognize either deprrecated or version 2+ only
+       syntax for specifying extensions.
+    """
+    # Get class name (if provided): `path.to.module:ClassName`
+    ext_name, class_name = extension_name.split(':', 1) \
+        if ':' in extension_name else (extension_name, '')
+
+    try:
+        module = importlib.import_module(ext_name)
+    except ImportError:
+        return {}
+    try:
+      if class_name:
+            return getattr(module, class_name).allowed_markup
+      else:
+            return module.allowed_markup
+    except AttributeError:
+        return {}
+
+def _get_base_bleach_kwargs():
     bleach_kwargs = {}
     bleach_kwargs['tags'] = [
         'p',
