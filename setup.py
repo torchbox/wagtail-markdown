c594962d3292e8aa70dedcd75876df38fa7ce09e
Dan Braghis <dan.braghis@torchbox.com>
2021-06-15 19:26:11 +0100

    Bump version to 0.7.0-rc2

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ CLASSIFIERS = [
 
 setup(
     name="wagtail-markdown",
-    version="0.7.0-rc1",
+    version="0.7.0-rc2",
     description="Markdown support for Wagtail",
     long_description="Provides Markdown page field and streamfield block for "
     "Wagtail. More info: {}".format(README),
