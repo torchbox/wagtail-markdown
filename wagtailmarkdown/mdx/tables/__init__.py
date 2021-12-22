343cd4fd2c42aa83dd0b875b707b23686c04a637
Nick Moreton <nick.moreton@torchbox.com>
2021-12-22 17:19:23 +0000

    Fix etree deprecation warnings (#93)
    Co-authored-by: tbrlpld "tibor@lpld.io"

--- wagtailmarkdown/mdx/tables/__init__.py
+++ wagtailmarkdown/mdx/tables/__init__.py
@@ -14,9 +14,19 @@ A simple example:
 Copyright 2009 - [Waylan Limberg](http://achinghead.com)
 """
 
+
+import markdown
+from packaging.version import Version
+
+if Version(markdown.__version__) >= Version("3.2"):
+    # Using markdown version 3.2+
+    import xml.etree.ElementTree as etree
+else:
+    # Using markdown below 3.2
+    from markdown.util import etree
+
 from markdown import Extension
 from markdown.blockprocessors import BlockProcessor
-from markdown.util import etree
 
 
 class TableProcessor(BlockProcessor):
