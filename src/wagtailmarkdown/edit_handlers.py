c3744b6a9b13e6a16e0501f0304abc379396f699
Dan Braghis <dan.braghis@torchbox.com>
2022-07-01 15:57:50 +0100

    Deprecate `wagtailmarkdown.edit_handlers.MarkdownPanel`

--- src/wagtailmarkdown/edit_handlers.py
+++ src/wagtailmarkdown/edit_handlers.py
@@ -1,5 +1,18 @@
+import warnings
+
 from wagtail.admin.edit_handlers import FieldPanel
+
+from wagtailmarkdown.warnings import WagtailMarkdownDeprecationWarning
 
 
 class MarkdownPanel(FieldPanel):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        warnings.warn(
+            (
+                "The `wagtailmarkdown.edit_handlers.MarkdownPanel` field panel is deprecated. "
+                "Please use the `FieldPanel()` instead."
+            ),
+            WagtailMarkdownDeprecationWarning,
+            stacklevel=2,
+        )
