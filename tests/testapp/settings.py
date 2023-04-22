b9e9f5e78665b91b56acf76a80222f1659c002f9
zerolab <dan@zerolab.org>
2023-04-22 16:34:32 +0100

    Tweak tox interactive mode

--- tests/testapp/settings.py
+++ tests/testapp/settings.py
@@ -6,6 +6,8 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 SECRET_KEY = "fake_secret_key_to_run_tests"  # pragma: allowlist secret
 
+IS_INTERACTIVE = "INTERACTIVE" in os.environ
+
 INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -19,7 +21,7 @@ INSTALLED_APPS = [
     "wagtail",
     "taggit",
     "wagtailmarkdown",
-    "tests.testapp",
+    "testapp" if IS_INTERACTIVE else "tests.testapp",
 ]
 
 TEMPLATES = [
@@ -44,7 +46,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_ROOT = os.path.join(PROJECT_DIR, "static")
 STATIC_URL = "/static/"
 
-ROOT_URLCONF = "tests.testapp.urls"
+ROOT_URLCONF = "testapp.urls" if IS_INTERACTIVE else "tests.testapp.urls"
 
 MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
@@ -63,4 +65,4 @@ WAGTAIL_SITE_NAME = "Wagtail Markdown Test"
 WAGTAILADMIN_BASE_URL = "http://localhost:8020/"
 WAGTAILDOCS_SERVE_METHOD = "direct"
 
-DEBUG = "INTERACTIVE" in os.environ
+DEBUG = IS_INTERACTIVE
