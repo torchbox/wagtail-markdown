4c688582cc4cd14756d2ba497a7bd65f728ce98e
zerolab <dan@zerolab.org>
2024-03-09 14:26:42 +0000

    Improve the logic for autodownloading FontAwesome

--- src/wagtailmarkdown/static/wagtailmarkdown/js/easymde.attach.js
+++ src/wagtailmarkdown/static/wagtailmarkdown/js/easymde.attach.js
@@ -22,7 +22,7 @@ function easymdeAttach(id, autoDownloadFontAwesome) {
     Object.assign(window.wagtailMarkdown.options, {
         element: document.getElementById(id),
         autofocus: false,
-        autoDownloadFontAwesome: autoDownloadFontAwesome !== null ? Boolean(autoDownloadFontAwesome) : null,
+        autoDownloadFontAwesome: autoDownloadFontAwesome,
     })
     var mde = new EasyMDE(window.wagtailMarkdown.options);
     mde.render();
